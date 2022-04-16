a073a5240c28e3f72f179c3f01db68d89eeff56c
Dharmendrakrjaitley <dharmendrajaitley@gmail.com>
2022-04-16 12:46:09 +0530

    first commit

---
+++ Print_name.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Print_name {
+
+	public static void main(String[] args) {
+		String name = "Dharmendra";
+		printName(1, name);
+
+	}
+
+	public static void printName(int i, String name) {
+		if (i > 5)
+			return;
+		System.out.println(name);
+
+		printName(i + 1, name);
+	}
+
+}
